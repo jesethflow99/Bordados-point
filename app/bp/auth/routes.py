12555282064e534c367b673d24eca133a8cff44a
Edgar <ceggarr199@gmail.com>
2024-11-16 01:18:21 -0600

    version 2

--- app/bp/auth/routes.py
+++ app/bp/auth/routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint,request,render_template,redirect,url_for,session
+from flask import Blueprint,request,render_template,redirect,url_for,session,flash
 from app.models import Usuario,db
 from app.encrypt import hashear_password,verificar_contraseña
 
@@ -8,16 +8,15 @@ auth=Blueprint("auth",__name__,url_prefix="/auth")
 def login():
     if request.method=="POST":
         nombre= request.form.get("nombre")
-        contasena=request.form.get("contrasena")
+        contrasena=request.form.get("contrasena")
         
         usuario=Usuario.query.filter(Usuario.Nombre_Usuario==nombre).first()
         
-        if usuario and verificar_contraseña(contasena,usuario.Contraseña):
+        if usuario and verificar_contraseña(contrasena,usuario.Contraseña):
             session["usuario_id"]=usuario.id
             session["Nombre_usuario"]=usuario.Nombre_Usuario
             print(session)
             return redirect(url_for("dashboard.index"))
-    
     return render_template("login.html")
 
 @auth.route("/logout")
@@ -25,23 +24,31 @@ def logout():
     session.pop('usuario_id', None)
     session.pop('Nombre_usuario', None)
     print(session)
-    return "deslogear Usuario"
+    return redirect(url_for("auth.login"))
 
 @auth.route("/register",methods=["POST","GET"])
 def register():
     if request.method== "POST":
         nombre= request.form.get("nombre")
-        contasena=request.form.get("contrasena")
+        contraseña=request.form.get("contrasena")
         
-        hash_cont=hashear_password(contasena)
-        usuario=Usuario.query.filter(Usuario.Nombre_Usuario==nombre).first()
-        if usuario:
-            return  "el usuario ya existe"
-        nuevo_usuario= Usuario(nombre,hash_cont)
-        db.session.add(nuevo_usuario)
-        db.session.commit()
-        print(session)
-        return redirect(url_for("auth.login"))
+        confcontrasena=request.form.get("confcontrasena")
+        
+        if contraseña == confcontrasena:
+            hash_cont=hashear_password(contraseña)
+            usuario=Usuario.query.filter(Usuario.Nombre_Usuario==nombre).first()
+            if usuario:
+                return  "el usuario ya existe"
+            nuevo_usuario= Usuario(nombre,hash_cont)
+            db.session.add(nuevo_usuario)
+            db.session.commit()
+            print(session)
+            flash("Usuario Registrado con exito","success")
+            return redirect(url_for("auth.login"))
+        else:
+            flash("Las contraseñas no coinciden", "danger")
+            return render_template("register.html")
+            
     
     return render_template("register.html")
 
--- app/bp/dashboard/routes.py
+++ app/bp/dashboard/routes.py
@@ -1,14 +1,18 @@
 from flask import Blueprint,request,render_template,redirect,url_for,session
+import app.bp.auth.routes
 from app.models import Usuario,db
+
 
 dashboard=Blueprint("dashboard",__name__,url_prefix="/dashboard")
 
 @dashboard.route("/inicio")
 def index():
+
     if session:
         mess=True
         nombre=session["Nombre_usuario"]
+        return render_template("index.html",nombre =nombre)
     else:
         mess=False
-        
-    return render_template("index.html",nombre=nombre)
+        return redirect(url_for("auth.login"))
+    
